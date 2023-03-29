2ed320871d4203c530412d5f3259fbe72f60cf1a
Kangjunesu <kulbit92@naver.com>
2023-03-29 19:32:10 +0900

    app.py에 삭제

--- app.py
+++ app.py
@@ -8,9 +8,27 @@ def home():
 @app.route('/toon')
 def review():
     return render_template('toon.html')
+##################################################################
+@app.route("/review:commentid", methods=["POST"])
+def comment_del():
 
+    del_pw = request.form['pw_give']
+    del_id = request.form['commentid_give']
 
-# ===========
+    # print(del_id)   
+    # print(del_pw) 
+    find_one = db.comment.find_one({'_id': ObjectId(del_id),'pw':del_pw})
+
+    # print(find_one)
+
+    if (None != find_one):       
+        # print("PW가 일치합니다.")
+        db.comment.delete_one({'_id': ObjectId(del_id),'pw':del_pw})
+        return jsonify({'msg':'삭제완료'})
+    else:
+        #  print("pw가 일치하지 않습니다.")
+         return jsonify({'msg':'pw가 일치하지 않습니다.'})    
+#######################################################################
 
 @app.route("/review", methods=["GET"])
 def movie_get():
