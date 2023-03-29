ae49754736d863123baab04225c10793fe389a96
Leezero3 <fuddl4525@naver.com>
2023-03-29 18:26:59 +0900

    메인페이지 라우터 변경

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ def home():
 
 @app.route('/toon')
 def review():
-    return render_template('review.html')
+    return render_template('toon.html')
 
 
 # ===========
-@app.route("/review", methods=["POST"])
-def movie_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg':'POST 연결 완료!'})
 
 @app.route("/review", methods=["GET"])
 def movie_get():
